40f4f5c6148e1339465c99cc48caca5fc5c67c5c
Mallika Parepalli <rkp2.net@gmail.com>
2025-01-10 18:16:42 +0530

    firstcommit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [ new HtmlWebpackPlugin({ template: './src/index.html', }), ],
+    devServer: {
+        //contentBase: './dist',
+        static: { directory: path.resolve(__dirname, 'dist'), },
+         compress: true, 
+         port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            { 
+            test: /\.(js|jsx)$/, 
+            exclude: /node_modules/, 
+            loader: "babel-loader" 
+        },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+}
