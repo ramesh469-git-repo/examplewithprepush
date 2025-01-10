b4730ab167305fa7b76b7809e3a5745979fbfd6c
Mallika Parepalli <rkp2.net@gmail.com>
2025-01-10 19:41:38 +0530

    Testing pre-push hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
 
@@ -8,6 +7,5 @@ export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
