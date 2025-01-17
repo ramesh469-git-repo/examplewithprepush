f1d7015cecad57a27e9bd867f43108c54556e7d0
Mallika Parepalli <rkp2.net@gmail.com>
2025-01-17 14:33:15 +0530

    Testing pre-push hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import("eslint").Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,jsx}"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: globals.browser
+  },
+  plugins: {
+    react: pluginReact
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "react/prop-types": "off"
+  }
+};
