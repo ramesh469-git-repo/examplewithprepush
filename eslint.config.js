170feda56ece662c660d9642e224924db017bb37
Mallika Parepalli <rkp2.net@gmail.com>
2025-01-17 14:59:55 +0530

    Testing pre-push hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,30 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import babelParser from '@babel/eslint-parser';  // Import the parser
 
 /** @type {import("eslint").Linter.Config} */
 export default {
-  files: ["**/*.{js,mjs,cjs,jsx}"],
+  files: [
+    "**/*.{js,mjs,cjs,jsx}",
+  ],
   languageOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    globals: globals.browser
+    globals: globals.browser,
+    parser: babelParser,  // Use the imported parser
+    parserOptions: {
+      babelOptions: {
+        presets: ['@babel/preset-react']
+      },
+      requireConfigFile: false,
+    },
   },
   plugins: {
-    react: pluginReact
+    react: pluginReact,
   },
   rules: {
     "no-unused-vars": "warn",
-    "react/prop-types": "off"
-  }
+    "react/prop-types": "off",
+  },
+  ignores: ["dist/**"],
 };
--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-// eslint-disable-next-line no-use-before-define
-import React from "react";//// eslint-disable-next-line no-use-before-define
+
+import React from "react";
 
 const App = () => {
     return (
-        <div>
+        <>
             <h1>Hello, World!</h1>
             <h2></h2>
-        </div>
+        </>
     );
 };
 
