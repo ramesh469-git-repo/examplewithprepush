d87c53a435c6d8e36b9e1ffdb27694d98993c810
Mallika Parepalli <rkp2.net@gmail.com>
2025-01-17 16:36:47 +0530

    Testing pre-push hook

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "!**/node_modules/**",
+      "src/**/*.(js|jsx|ts|tsx)",
+      "!src/**/*.test.(js|jsx|ts|tsx)"
+    ],
+    coverageReporters: ["json", "lcov", "text", "clover"],
+    testEnvironment: "jest-environment-jsdom",
+    "testMatch": [
+        "**/__tests__/**/*.(js|jsx|ts|tsx)",
+        "**/?(*.)+(spec|test).(js|jsx|ts|tsx)"
+      ],
+  };
+  
--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+// eslint-disable-next-line no-unused-vars
 import App from './App';
 
 test("renders Hello, World! heading", () => {
